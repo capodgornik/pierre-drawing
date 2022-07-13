876c3925ff5a36f04d0f91ee3c43d815c13cc4ea
Celine Podgornik <31493506+capodgornik@users.noreply.github.com>
2022-07-13 13:47:01 -0700

    Create pierreDrawingRevised.py

---
+++ pierreDrawingRevised.py
@@ -0,0 +1,3 @@
+# One day I would like to revist the code from the original drawing of Pierre
+# because there is a lot that can be cleaned up. But it was one of my first times
+# writing code, so I'm still super proud of it!
